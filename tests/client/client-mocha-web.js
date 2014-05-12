a997136152a47cf5d0fc7fba552626ed2283564a
Josh Owens <joshua.owens@gmail.com>
2014-05-12 15:10:34 -0400

    Try to get mocha-web working with velocity

--- tests/client/client-mocha-web.js
+++ tests/client/client-mocha-web.js
@@ -1,11 +1,38 @@
+chai.should()
 
 describe("Episode", function() {
+  var episode;
+
+  before(function() {
+    episode = Episode.create({title: 'Episode 15 - Google Ventures'});
+  });
+
+  after(function() {
+    episode.destroy();
+  });
+
   describe("slugify on creation", function() {
     it("creates a slug from the title", function() {
-      Episode.create({title: 'Episode 15 - Google Ventures'})
       episode = Episode.first({title: 'Episode 15 - Google Ventures'})
-      chai.assert.equal('episode-15-google-ventures', episode.slug);
-      episode.destroy
+      episode.slug.should.equal('episode-15-google-ventures');
+    });
+  });
+
+  describe("should display on the episodes template", function() {
+    var div;
+    before(function() {
+      div = document.createElement("DIV");
+      comp = UI.renderWithData(Template.episode, episode);
+      UI.insert(comp, div);
+    });
+
+    it("Title", function() {
+      $(div).html().should.include('Episode 15 - Google Ventures');
+    });
+
+    it("with an episode link", function() {
+      link = '/episode/episode-15-google-ventures';
+      $(div).find('a').prop('href').should.include(link);
     });
   });
 });
